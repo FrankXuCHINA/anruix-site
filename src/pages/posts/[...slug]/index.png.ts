171d4c3409b365528c2405bf87874ebb4bf865e5
Anruix <tel18251101019@gmail.com>
2026-07-11 16:40:19 +0800

    style: adopt Apple-inspired visual theme

--- src/pages/posts/[...slug]/index.png.ts
+++ src/pages/posts/[...slug]/index.png.ts
@@ -49,7 +49,8 @@ export const GET: APIRoute = async ({ props, url }) => {
       type: "div",
       props: {
         style: {
-          background: "#fefbfb",
+          background: "#ffffff",
+          color: "#1d1d1f",
           width: "100%",
           height: "100%",
           display: "flex",
@@ -64,10 +65,9 @@ export const GET: APIRoute = async ({ props, url }) => {
                 position: "absolute",
                 top: "-1px",
                 right: "-1px",
-                border: "4px solid #000",
-                background: "#ecebeb",
-                opacity: "0.9",
-                borderRadius: "4px",
+                border: "2px solid #d2d2d7",
+                background: "#f5f5f7",
+                borderRadius: "24px",
                 display: "flex",
                 justifyContent: "center",
                 margin: "2.5rem",
@@ -80,9 +80,9 @@ export const GET: APIRoute = async ({ props, url }) => {
             type: "div",
             props: {
               style: {
-                border: "4px solid #000",
-                background: "#fefbfb",
-                borderRadius: "4px",
+                border: "2px solid #d2d2d7",
+                background: "#ffffff",
+                borderRadius: "24px",
                 display: "flex",
                 justifyContent: "center",
                 margin: "2rem",
@@ -93,6 +93,7 @@ export const GET: APIRoute = async ({ props, url }) => {
                 type: "div",
                 props: {
                   style: {
+                    color: "#1d1d1f",
                     display: "flex",
                     flexDirection: "column",
                     justifyContent: "space-between",
@@ -140,6 +141,7 @@ export const GET: APIRoute = async ({ props, url }) => {
                                   type: "span",
                                   props: {
                                     style: {
+                                      color: "#0071e3",
                                       overflow: "hidden",
                                       fontWeight: "bold",
                                     },
@@ -152,7 +154,11 @@ export const GET: APIRoute = async ({ props, url }) => {
                           {
                             type: "span",
                             props: {
-                              style: { overflow: "hidden", fontWeight: "bold" },
+                              style: {
+                                color: "#6e6e73",
+                                overflow: "hidden",
+                                fontWeight: "bold",
+                              },
                               children: config.site.title,
                             },
                           },
